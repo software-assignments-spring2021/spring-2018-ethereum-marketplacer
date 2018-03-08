9c56ede21ffd8222ebe79731abbe37943bcdd8ff
chris-chao <christopherchao97@gmail.com>
2018-03-08 00:16:47 -0500

    Changed some directory structure for more standard React setup. Added .css for App.js Changed href linking other pages to React component calling

--- src/App.js
+++ src/App.js
@@ -1,38 +1,63 @@
 import React, {Component} from 'react'
-import Post from './post.js'
+import Post from './components/Post.js'
 import Home from './home.js'
+import './css/App.css';
+
+
 
 class App extends Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
-            route: null
+            route: null,
+            showPostComponent: false
+        };
+        this.togglePostComponent = this.togglePostComponent.bind(this);
+
+    }
+
+  componentDidMount() {
+    // window.addEventListener('hashchange', () => {
+    //   this.setState({route: window.location.hash.substr(1)})
+    // });
+  }
+
+    togglePostComponent() {
+        {this.state.showPostComponent ?
+            this.setState({showPostComponent: false})
+        :
+            this.setState({showPostComponent: true})
         }
     }
-  componentDidMount() {
-    window.addEventListener('hashchange', () => {
-      this.setState({route: window.location.hash.substr(1)})
-    })
-  }
-  render(){
-    let Child
-    switch(this.state.route) {
-        case '/post' : Child = Post;
-        break;
-        default : Child = Home;
-    }
+
+
+  render() {
+    // let Child;
+    // switch(this.state.route) {
+    //     case '/post' : Child = Post;
+    //     break;
+    //     default : Child = Home;
+    // }
+
     return (
-      <div>
-        <h1>
-          Question & Answer
-        </h1>
-        <ul>
-          <li><a href='#/index'>Home</a></li>
-          <li><a href='#/post'>Question</a></li>
-        </ul>
-        <Child />
+      <div className="App">
+          <header className="App-header">
+              <h1 className="App-title">QuestionAnswer</h1>
+              <button onClick={this.togglePostComponent} className="Post-button" >Post Question</button>
+          </header>
+
+          {/*This is where the main panel is displayed*/}
+          <div className="Main-panel">
+              {this.state.showPostComponent ? <Post/> : null}
+
+              {/*Need to display QuestionList component here */}
+
+
+
+
+          </div>
       </div>
     )
   }
 }
-export default App
+export default App;
