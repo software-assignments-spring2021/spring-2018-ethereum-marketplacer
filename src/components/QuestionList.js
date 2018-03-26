19e6e54cdd2e9fd097ccb5e27fcd83a09366c20a
chris-chao <christopherchao97@gmail.com>
2018-03-26 14:15:13 -0400

    Converted unix epoch timestamp to regular date and time Converted bounty unit display in gwei to eth

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -55,6 +55,16 @@ class QuestionList extends Component {
         })
     }
 
+    epochToDate(epochTimestamp) {
+        let d = new Date(0); // The 0 there is the key, which sets the date to the epoch
+        d.setUTCSeconds(epochTimestamp);
+        return d.toDateString() + " " + d.toLocaleTimeString();;
+    }
+
+    gweiToEth(bountyInGwei){
+        return bountyInGwei/1000000000000000000 + " ETH";
+    }
+
     renderQuestionList() {
         // questions is an array of questions
         let questions = this.state.questions;
@@ -68,17 +78,16 @@ class QuestionList extends Component {
                 )} className="Individual-Question-Title"> {question.questionTitle}
                 </div>
                 {/*<div className="Individual-Question-Description">*/}
-                    {/*Description: {question.questionDescription}*/}
+                {/*Description: {question.questionDescription}*/}
                 {/*</div>*/}
                 <div className="Individual-Question-Bounty">
-                    Bounty: {question.bounty.toNumber()}
+                    Bounty: {this.gweiToEth(question.bounty.toNumber())}
                 </div>
                 <div className="Individual-Question-Time">
-                    Time Submitted: {question.timestamp.toNumber()}
+                    Time Submitted: {this.epochToDate(question.timestamp.toNumber())}
                 </div>
                 <hr/>
             </div>
-
         );
 
         return (
@@ -90,8 +99,7 @@ class QuestionList extends Component {
     }
 
     toggleSingleQuestionComponent = (questionID, questionTitle, questionDesc, questionBounty, questionTimestamp) => {
-        this.props.toggleSingleQuestion(questionID, questionTitle, questionDesc, questionBounty, questionTimestamp);
-
+        this.props.toggleSingleQuestion(questionID, questionTitle, questionDesc, this.gweiToEth(questionBounty), this.epochToDate(questionTimestamp));
     };
 
 
