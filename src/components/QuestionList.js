635d252e38d7b3fbeec673400e5f7b7a648e04d9
Chris Chao <christopherchao97@gmail.com>
2018-03-26 15:32:22 -0400

    Fixed styling in all components, unixToDate, gweiToEth (#83)
    * User can now click on questionTitle from QuestionList to view SingleQuestion component
    SingleQuestion component contains all information from clicked question
    SingleQuestion component contains markup of submitReply form, allRepliesList
    SingleQuestion component is NOT styled yet...
    
    * Fixed up styling for UI
    
    * Converted unix epoch timestamp to regular date and time
    Converted bounty unit display in gwei to eth

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -55,6 +55,16 @@ class QuestionList extends Component {
         })
     }
 
+    epochToDate(epochTimestamp) {
+        let d = new Date(0); // The 0 there is the key, which sets the date to the epoch
+        d.setUTCSeconds(epochTimestamp);
+        return d.toDateString() + " " + d.toLocaleTimeString();;
+    }
+
+    gweiToEth(bountyInGwei){
+        return bountyInGwei/1000000000000000000 + " ETH";
+    }
+
     renderQuestionList() {
         // questions is an array of questions
         let questions = this.state.questions;
@@ -67,16 +77,16 @@ class QuestionList extends Component {
                     question.timestamp.toNumber()
                 )} className="Individual-Question-Title"> {question.questionTitle}
                 </div>
-                <div className="Individual-Question-Description">
-                    Description: {question.questionDescription}
-                </div>
+                {/*<div className="Individual-Question-Description">*/}
+                {/*Description: {question.questionDescription}*/}
+                {/*</div>*/}
                 <div className="Individual-Question-Bounty">
-                    Bounty: {question.bounty.toNumber()}
+                    Bounty: {this.gweiToEth(question.bounty.toNumber())}
                 </div>
                 <div className="Individual-Question-Time">
-                    Time Submitted: {question.timestamp.toNumber()}
+                    Time Submitted: {this.epochToDate(question.timestamp.toNumber())}
                 </div>
-
+                <hr/>
             </div>
         );
 
@@ -89,8 +99,7 @@ class QuestionList extends Component {
     }
 
     toggleSingleQuestionComponent = (questionID, questionTitle, questionDesc, questionBounty, questionTimestamp) => {
-        this.props.toggleSingleQuestion(questionID, questionTitle, questionDesc, questionBounty, questionTimestamp);
-
+        this.props.toggleSingleQuestion(questionID, questionTitle, questionDesc, this.gweiToEth(questionBounty), this.epochToDate(questionTimestamp));
     };
 
 
